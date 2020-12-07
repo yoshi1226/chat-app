d48dec15d69fa2399a25d143a2d28295ddf3d26c
yoshi1226 <75423226+yoshi1226@users.noreply.github.com>
2020-12-07 13:12:34 +0900

    Revert "コミット"
    This reverts commit cf0f189c567750ec5f10c0808d9783d36c803424.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   devise_for :users
+=======
   get 'messages/index'
+>>>>>>> master
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "messages#index"
-  resources :users, only: [:edit, :update]
-
 end
